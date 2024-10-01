9578198bc521657d9154e5f7c3a6881a5debcc2c
Guilherme Hermes <85593021+GuilhermeHermes@users.noreply.github.com>
2024-10-01 11:10:24 -0300

    fix: workflow

--- src/main/java/com/guilhermehermes/urlshortener/UrlshortenerApplication.java
+++ src/main/java/com/guilhermehermes/urlshortener/UrlshortenerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@SpringBootApplication
+@EnableMongoRepositories
 public class UrlshortenerApplication {
 
     public static void main(String[] args) {
