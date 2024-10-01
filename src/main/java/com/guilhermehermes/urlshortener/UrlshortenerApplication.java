73a7e6ae138429b577a97c639cd56ddcc9ed4125
Guilherme Hermes <85593021+GuilhermeHermes@users.noreply.github.com>
2024-10-01 10:53:18 -0300

    add CI workflow

--- src/main/java/com/guilhermehermes/urlshortener/UrlshortenerApplication.java
+++ src/main/java/com/guilhermehermes/urlshortener/UrlshortenerApplication.java
@@ -1,9 +1,11 @@
 package com.guilhermehermes.urlshortener;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class UrlshortenerApplication {
 
     public static void main(String[] args) {
